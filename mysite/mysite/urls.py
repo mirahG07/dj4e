acf3c08a8473b21b9a5dcb140bb5e04598282da1
mrw <lwilmoth>
2024-11-22 10:54:52 -0500

    added autos

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,12 +19,15 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
 from django.views.generic import TemplateView
+#from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 # Up two folders to serve "site" content
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SITE_ROOT = os.path.join(BASE_DIR, 'site')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('polls/', include('polls.urls')),
     path('hello/', include('hello.urls')),
     path('autos/', include('autos.urls')),
@@ -33,4 +36,6 @@ urlpatterns = [
         name='site_path'
     ),
     path('', TemplateView.as_view(template_name='home/main.html')),
+    path("accounts/", include("django.contrib.auth.urls")),
+    #url(r'^oath/', include('social_django.urls', namespace='social')),
 ]
